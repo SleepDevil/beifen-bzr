b6137f8b7057f2d5bbc542733d0b466a7c821615
SleepDevil <1752915704@qq.com>
2021-01-10 11:21:49 +0800

    init repo

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-xl" id="app">
+    姓名： {{ name }}
+    年龄：
+    <button @click="changeAge(-1)">-</button>
+    {{ age }}
+    <button @click="changeAge(1)">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { ConfigProvider } from 'ant-design-vue';
+
+  export default defineComponent({
+    name: 'App',
+    components: { ConfigProvider },
+    props: {
+      // t: String,
+    },
+    setup() {
+      return {};
+    },
+  });
+</script>
---
+++ src/types/vue-app-env.d.ts
@@ -0,0 +1,17 @@
+declare module '*.vue' {
+  import { defineComponent } from 'vue';
+  const Component: ReturnType<typeof defineComponent>;
+  export default Component;
+}
+
+declare namespace NodeJS {
+  interface Process {
+    env: ProcessEnv;
+  }
+
+  interface Processenv {
+    readonly NODE_ENV: 'development' | 'production';
+  }
+}
+
+declare let process: NodeJS.Process
